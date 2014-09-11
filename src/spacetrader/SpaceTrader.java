9b9f8b8f5304f5f4353b316a2a1dd4c35fec0088
Tejas <Tejas@128.61.47.245>
2014-09-11 18:08:43 -0400

    Settings and Set name

--- src/spacetrader/SpaceTrader.java
+++ src/spacetrader/SpaceTrader.java
@@ -12,9 +12,10 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
 /**
  *
@@ -28,13 +29,7 @@ public class SpaceTrader extends Application {
         newGame.setText("Start a new game!");
         Button reload = new Button();
         reload.setText("Reload a previous game!");
-        newGame.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                //Handle new game start here
-            }
-        });
+
         
         VBox root = new VBox();
         root.setPadding(new Insets(10));
@@ -42,11 +37,39 @@ public class SpaceTrader extends Application {
         root.getChildren().add(newGame);
         root.getChildren().add(reload);
         
-        Scene scene = new Scene(root, 300, 250);
-        
+        Scene scene = new Scene(root, 300, 250);     
         primaryStage.setTitle("Space Traders!");
         primaryStage.setScene(scene);
+        
         primaryStage.show();
+                newGame.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                //Handle new game start here
+                root.getChildren().clear();
+                TextField name = new TextField();
+                name.setText("Type in Your Name.");
+                Label difficulty = new Label();
+                difficulty.setText("Select Difficulty: ");
+                Button easy = new Button();
+                easy.setText("Easy Mode");
+                Button medium = new Button();
+                medium.setText("Medium Mode");
+                Button hard = new Button();
+                hard.setText("Hard Mode");
+                Button ultra = new Button();
+                ultra.setText("Ultra Mode");
+                root.getChildren().add(name);
+                root.getChildren().add(difficulty);
+                root.getChildren().add(easy);
+                root.getChildren().add(medium);
+                root.getChildren().add(hard);
+                root.getChildren().add(ultra);
+                
+            }
+        });
+                
     }
 
     /**
