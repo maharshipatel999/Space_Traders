a56568e98192e8e29ca7ce7f2496e8d91afa43f1
Maharshi Patel <maharshipatel999@gmail.com>
2014-09-21 19:54:55 -0400

    Created randomizing universe. Waiting on TechLevel and Resources enums to be added

--- src/spacetrader/SolarSystem.java
+++ src/spacetrader/SolarSystem.java
@@ -6,28 +6,31 @@
 
 package spacetrader;
 
+import java.awt.Point;
+import java.util.Random;
+
 /**
  *
  * @author maharshipatel999
  */
 public enum SolarSystem {
     Acamar,
-    Adahn,		// The alternate personality for The Nameless One in "Planescape: Torment"
+    Adahn,      // The alternate personality for The Nameless One in "Planescape: Torment"
     Aldea,
     Andevian,
     Antedi,
     Balosnee,
     Baratas,
-    Brax,			// One of the heroes in Master of Magic
-    Bretel,		// This is a Dutch device for keeping your pants up.
+    Brax,           // One of the heroes in Master of Magic
+    Bretel,     // This is a Dutch device for keeping your pants up.
     Calondia,
     Campor,
-    Capelle,		// The city I lived in while programming this game
+    Capelle,        // The city I lived in while programming this game
     Carzon,
-    Castor,		// A Greek demi-god
+    Castor,     // A Greek demi-god
     Cestus,
-    Cheron,		
-    Courteney,	// After Courteney Cox…
+    Cheron,     
+    Courteney,  // After Courteney Cox…
     Daled,
     Damast,
     Davlos,
@@ -37,98 +40,114 @@ public enum SolarSystem {
     Draylon,
     Drema,
     Endor,
-    Esmee,		// One of the witches in Pratchett's Discworld
+    Esmee,      // One of the witches in Pratchett's Discworld
     Exo,
-    Ferris,		// Iron
-    Festen,		// A great Scandinavian movie
-    Fourmi,		// An ant, in French
-    Frolix,		// A solar system in one of Philip K. Dick's novels
+    Ferris,     // Iron
+    Festen,     // A great Scandinavian movie
+    Fourmi,     // An ant, in French
+    Frolix,     // A solar system in one of Philip K. Dick's novels
     Gemulon,
-    Guinifer,		// One way of writing the name of king Arthur's wife
-    Hades,		// The underworld
-    Hamlet,		// From Shakespeare
-    Helena,		// Of Troy
-    Hulst,		// A Dutch plant
-    Iodine,		// An element
+    Guinifer,       // One way of writing the name of king Arthur's wife
+    Hades,      // The underworld
+    Hamlet,     // From Shakespeare
+    Helena,     // Of Troy
+    Hulst,      // A Dutch plant
+    Iodine,     // An element
     Iralius,
-    Janus,		// A seldom encountered Dutch boy's name
+    Janus,      // A seldom encountered Dutch boy's name
     Japori,
     Jarada,
-    Jason,		// A Greek hero
+    Jason,      // A Greek hero
     Kaylon,
     Khefka,
-    Kira,			// My dog's name
-    Klaatu,		// From a classic SF movie
+    Kira,           // My dog's name
+    Klaatu,     // From a classic SF movie
     Klaestron,
-    Korma,		// An Indian sauce
-    Kravat,		// Interesting spelling of the French word for "tie"
+    Korma,      // An Indian sauce
+    Kravat,     // Interesting spelling of the French word for "tie"
     Krios,
-    Laertes,		// A king in a Greek tragedy
+    Laertes,        // A king in a Greek tragedy
     Largo,
-    Lave,			// The starting system in Elite
+    Lave,           // The starting system in Elite
     Ligon,
-    Lowry,		// The name of the hero in Terry Gilliam's Brazil
-    Magrat,		// The second of the witches in Pratchett's Discworld
+    Lowry,      // The name of the hero in Terry Gilliam's Brazil
+    Magrat,     // The second of the witches in Pratchett's Discworld
     Malcoria,
     Melina,
-    Mentar,		// The Psilon home system in Master of Orion
+    Mentar,     // The Psilon home system in Master of Orion
     Merik,
     Mintaka,
-    Montor,		// A city in Ultima III and Ultima VII part 2
+    Montor,     // A city in Ultima III and Ultima VII part 2
     Mordan,
-    Myrthe,		// The name of my daughter
+    Myrthe,     // The name of my daughter
     Nelvana,
-    Nix,			// An interesting spelling of a word meaning nothing in Dutch
-    Nyle,			// An interesting spelling of the great river
+    Nix,            // An interesting spelling of a word meaning nothing in Dutch
+    Nyle,           // An interesting spelling of the great river
     Odet,
-    Og,			// The last of the witches in Pratchett's Discworld
-    Omega,		// The end of it all
-    Omphalos,		// Greek for navel
+    Og,         // The last of the witches in Pratchett's Discworld
+    Omega,      // The end of it all
+    Omphalos,       // Greek for navel
     Orias,
-    Othello,		// From Shakespeare
-    Parade,		// This word means the same in Dutch and in English
+    Othello,        // From Shakespeare
+    Parade,     // This word means the same in Dutch and in English
     Penthara,
-    Picard,		// The enigmatic captain from ST:TNG
-    Pollux,		// Brother of Castor
+    Picard,     // The enigmatic captain from ST:TNG
+    Pollux,     // Brother of Castor
     Quator,
     Rakhar,
-    Ran,			// A film by Akira Kurosawa
+    Ran,            // A film by Akira Kurosawa
     Regulas,
     Relva,
     Rhymus,
     Rochani,
-    Rubicum,		// The river Ceasar crossed to get into Rome
+    Rubicum,        // The river Ceasar crossed to get into Rome
     Rutia,
     Sarpeidon,
     Sefalla,
     Seltrice,
     Sigma,
-    Sol,			// That's our own solar system
+    Sol,            // That's our own solar system
     Somari,
     Stakoron,
     Styris,
     Talani,
     Tamus,
-    Tantalos,		// A king from a Greek tragedy
+    Tantalos,       // A king from a Greek tragedy
     Tanuga,
     Tarchannen,
     Terosa,
-    Thera,		// A seldom encountered Dutch girl's name
-    Titan,		// The largest moon of Jupiter
-    Torin,		// A hero from Master of Magic
+    Thera,      // A seldom encountered Dutch girl's name
+    Titan,      // The largest moon of Jupiter
+    Torin,      // A hero from Master of Magic
     Triacus,
     Turkana,
     Tyrus,
-    Umberlee,		// A god from AD&D, which has a prominent role in Baldur's Gate
-    Utopia,		// The ultimate goal
+    Umberlee,       // A god from AD&D, which has a prominent role in Baldur's Gate
+    Utopia,     // The ultimate goal
     Vadera,
     Vagra,
     Vandor,
     Ventax,
     Xenon,
-    Xerxes,		// A Greek hero
-    Yew,			// A city which is in almost all of the Ultima games
-    Yojimbo,		// A film by Akira Kurosawa
+    Xerxes,     // A Greek hero
+    Yew,            // A city which is in almost all of the Ultima games
+    Yojimbo,        // A film by Akira Kurosawa
     Zalkon,
-    Zuul			// From the first Ghostbusters movie
+    Zuul;           // From the first Ghostbusters movie
+    
+//    private final Point location;
+//    private final TechLevel level;
+//    private final Resource resource;
+//    private final PoliticalSystem government;
+    
+    SolarSystem() {
+        Random rand = new Random();
+        for (SolarSystem s : SolarSystem.values()) {
+//            location = new Point(rand.nextInt() + 10, rand.nextInt() + 10);
+//            level =
+//            resource =
+//            government = 
+        }
+    }
+
 }
