f172063f0ec46802597f708b074e177206867d52
Caleb Stokols <cjstokols@gmail.com>
2014-10-20 17:37:46 -0400

    Merged with Seth's changes

--- src/spacetrader/travel/ChangeHullEvent.java
+++ src/spacetrader/travel/ChangeHullEvent.java
@@ -13,9 +13,18 @@ import spacetrader.Player;
  * @author Seth
  */
 public class ChangeHullEvent extends RandomEvent {
+    /**
+     * Constructor for ChangeHullEvent
+     * @param player
+     * @param message
+     * @param quantityChange 
+     */
     public ChangeHullEvent(Player player, String message, int quantityChange) {
         super(player, message, quantityChange);
     }
+    /**
+     * Sets hullstrength to hullstreth + quantity change
+     */
     @Override
     public void doEvent() {
         player.getShip().setHullStrength(player.getShip().getHullStrength() + quantityChange);
