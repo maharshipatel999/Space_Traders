0bbf5f6f58b75d5d395b3e161ede914fd23429d9
Caleb Stokols <cjstokols@gmail.com>
2014-11-03 21:10:17 -0500

    Fixed Gadget class

--- src/spacetrader/ships/Gadget.java
+++ src/spacetrader/ships/Gadget.java
@@ -6,27 +6,19 @@
 
 package spacetrader.ships;
 
-import spacetrader.planets.TechLevel;
-
 /**
  *
  * @author nkaru_000
  */
-public enum Gadget {
+public class Gadget {
     
-    EXTRA_CARGO        (TechLevel.MEDIEVAL, 200, "5 Extra Cargo Bays"),
-    NAVIGATION         (TechLevel.POST_INDUSTRIAL, 1000, "Navigating System"),
-    AUTO_REPAIR        (TechLevel.RENAISSANCE, 750, "Auto-Repair System"),
-    TARGETING          (TechLevel.POST_INDUSTRIAL, 9000, "Targeting System"),
-    CLOAK              (TechLevel.HI_TECH, 20000, "Cloaking Device");
+    private GadgetType type;
     
-    private TechLevel minTechLevel;
-    private int price;
-    private String name;
+    public Gadget(GadgetType type) {
+        this.type = type;
+    }
     
-    private Gadget(TechLevel minTechLevel, int price, String name) {
-        this.minTechLevel = minTechLevel;
-        this.price = price;
-        this.name = name;
+    public GadgetType getType() {
+        return type;
     }
 }
