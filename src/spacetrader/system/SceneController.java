afe1b520cc0b04465ddfb14189b5bc67221974cb
Naveena <nkarusala3@gatech.edu>
2014-12-04 14:31:05 -0500

    added css and UI changes

--- src/spacetrader/system/SceneController.java
+++ src/spacetrader/system/SceneController.java
@@ -33,6 +33,7 @@ public abstract class SceneController {
      */
     public void setScene(Scene scene) {
         this.rootScene = scene;
+        rootScene.getStylesheets().add("/spacetrader/systemStyle.css");
     }
     
     /**
